6317dd3029c2e119c0ef0122e00e0b73907fd816
huai23 <huai23@qq.com>
2018-05-29 23:09:17 +0800

    约课

--- src/main/java/com/training/service/CoachRestService.java
+++ src/main/java/com/training/service/CoachRestService.java
@@ -4,6 +4,7 @@ import com.training.dao.*;
 import com.training.entity.*;
 import com.training.domain.User;
 import com.training.common.*;
+import com.training.util.IDUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -33,6 +34,7 @@ public class CoachRestService {
      */ 
     public ResponseEntity<String> add(CoachRestEntity coachRest){
         User user = RequestContextHelper.getUser();
+        coachRest.setRestId(IDUtils.getId());
         int n = coachRestDao.add(coachRest);
         if(n==1){
             return ResponseUtil.success("添加成功");
