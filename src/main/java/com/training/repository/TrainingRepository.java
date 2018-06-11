d27330786021f33ab14e038a1ee9607e3df421d9
huai23 <huai23@qq.com>
2018-06-11 23:02:28 +0800

    约课

--- src/main/java/com/training/repository/TrainingRepository.java
+++ src/main/java/com/training/repository/TrainingRepository.java
@@ -92,8 +92,8 @@ public interface TrainingRepository {
             " <if test=\"query.cardNo != null\"> AND card_no = #{query.cardNo} </if>" +
             " <if test=\"query.cardType != null\"> AND card_type = #{query.cardType} </if>" +
             " <if test=\"query.lessonDate != null\"> AND lesson_date = #{query.lessonDate} </if>" +
-            " <if test=\"query.startDate != null\"> AND lessonDate &gt;= #{query.startDate} </if>" +
-            " <if test=\"query.endDate != null\"> AND lessonDate &lt;= #{query.endDate} </if>" +
+            " <if test=\"query.startDate != null\"> AND lesson_date &gt;= #{query.startDate} </if>" +
+            " <if test=\"query.endDate != null\"> AND lesson_date &lt;= #{query.endDate} </if>" +
             " <if test=\"query.startHour != null\"> AND start_hour = #{query.startHour} </if>" +
             " <if test=\"query.endHour != null\"> AND end_hour = #{query.endHour} </if>" +
             " <if test=\"query.trainingData != null\"> AND training_data = #{query.trainingData} </if>" +
