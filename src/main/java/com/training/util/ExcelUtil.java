c5aae56a06e48c67557fb6585af6e3013e75c926
huai23@qq.com <huai2323>
2018-06-28 02:09:28 +0800

    反馈增加类型字段

---
+++ src/main/java/com/training/util/ExcelUtil.java
@@ -0,0 +1,153 @@
+package com.training.util;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.alibaba.fastjson.JSON;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.CellType;
+
+public class ExcelUtil {
+    //默认单元格内容为数字时格式
+    private static DecimalFormat df = new DecimalFormat("0");
+    // 默认单元格格式化日期字符串
+    private static SimpleDateFormat sdf = new SimpleDateFormat(  "yyyy-MM-dd HH:mm:ss");
+    // 格式化数字
+    private static DecimalFormat nf = new DecimalFormat("0.00");
+
+    public static List<List<String>> readExcel(File file){
+        if(file == null){
+            return null;
+        }
+        return readExcel2003(file);
+    }
+
+    /*
+     * @return 将返回结果存储在ArrayList内，存储结构与二位数组类似
+     * lists.get(0).get(0)表示过去Excel中0行0列单元格
+     */
+    public static List<List<String>> readExcel2003(File file){
+        try{
+            List<List<String>> rowList = new ArrayList<List<String>>();
+            List<String> colList;
+            HSSFWorkbook wb = new HSSFWorkbook(new FileInputStream(file));
+            HSSFSheet sheet = wb.getSheetAt(0);
+            HSSFRow row;
+            HSSFCell cell;
+            String value;
+            for(int i = sheet.getFirstRowNum() , rowCount = 0; rowCount < sheet.getPhysicalNumberOfRows() ; i++ ){
+                row = sheet.getRow(i);
+                colList = new ArrayList<>();
+                if(row == null){
+                    //当读取行为空时
+                    if(i != sheet.getPhysicalNumberOfRows()){//判断是否是最后一行
+                        rowList.add(colList);
+                    }
+                    continue;
+                }else{
+                    rowCount++;
+                }
+                for( int j = row.getFirstCellNum() ; j <= row.getLastCellNum() ;j++){
+                    cell = row.getCell(j);
+                    if(cell == null || cell.getCellType() == HSSFCell.CELL_TYPE_BLANK){
+                        //当该单元格为空
+                        if(j != row.getLastCellNum()){//判断是否是该行中最后一个单元格
+                            colList.add("");
+                        }
+                        continue;
+                    }
+                    cell.setCellType(CellType.STRING);
+                    value = cell.getRichStringCellValue().toString().trim();
+                    colList.add(value);
+                }//end for j
+                rowList.add(colList);
+            }//end for i
+
+            return rowList;
+        }catch(Exception e){
+            return null;
+        }
+    }
+
+    public static void writeExcel(List<List<String>> result, String path){
+        if(result == null){
+            return;
+        }
+        HSSFWorkbook wb = new HSSFWorkbook();
+        HSSFSheet sheet = wb.createSheet("sheet1");
+        for(int i = 0 ;i < result.size() ; i++){
+            HSSFRow row = sheet.createRow(i);
+            if(result.get(i) != null){
+                for(int j = 0; j < result.get(i).size() ; j ++){
+                    HSSFCell cell = row.createCell(j);
+                    cell.setCellType(CellType.STRING);
+                    cell.setCellValue(result.get(i).get(j));
+                }
+            }
+        }
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        try
+        {
+            wb.write(os);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        byte[] content = os.toByteArray();
+        File file = new File(path);//Excel文件生成后存储的位置。
+        OutputStream fos  = null;
+        try
+        {
+            fos = new FileOutputStream(file);
+            fos.write(content);
+            os.close();
+            fos.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public static DecimalFormat getDf() {
+        return df;
+    }
+    public static void setDf(DecimalFormat df) {
+        ExcelUtil.df = df;
+    }
+    public static SimpleDateFormat getSdf() {
+        return sdf;
+    }
+    public static void setSdf(SimpleDateFormat sdf) {
+        ExcelUtil.sdf = sdf;
+    }
+    public static DecimalFormat getNf() {
+        return nf;
+    }
+    public static void setNf(DecimalFormat nf) {
+        ExcelUtil.nf = nf;
+    }
+
+    public static void main(String[] args) {
+        File file = new File("/Users/huai23/Documents/健身/contract20180627.xls");
+        List<List<String>> data = readExcel(file);
+        int i = 0;
+        for (List<String> item : data){
+            System.out.println(JSON.toJSONString(item));
+            i++;
+        }
+        System.out.println(" count = "+i);
+
+
+    }
+
+
+}
