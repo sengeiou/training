cbdec54c688c7e60d2b90380d6ad782e8927e983
huai45 <huai23@qq.com>
2018-07-20 08:34:03 +0800

    https证书

--- src/main/java/com/training/api/UploadFileController.java
+++ src/main/java/com/training/api/UploadFileController.java
@@ -37,6 +37,12 @@ public class UploadFileController {
         logger.info(" fileName = {} ",fileName);
         logger.info(" path = {} ",path);
         fileName = IDUtils.getId()+"_"+fileName;
+
+        File dic = new File(path);
+        if(!dic.exists()){
+            dic.mkdirs();
+        }
+
         File targetFile = new File(path+"/"+fileName);
         File pathf = new File(path);
         if(!pathf.exists()){
@@ -58,6 +64,43 @@ public class UploadFileController {
         JSONObject jo = new JSONObject();
         jo.put("msg", "上传成功");
         jo.put("url", request.getContextPath()+"/upload/image/"+fileName);
+        return ResponseUtil.success(jo);
+    }
+
+    @RequestMapping(value = "staffImage")
+    public ResponseEntity<String> uploadStaffImage(@RequestParam(value = "file", required = false) MultipartFile file, HttpServletRequest request, ModelMap model) {
+        logger.info(" staffImage  start File.separator = {}",File.separator);
+        String path = request.getSession().getServletContext().getRealPath("/upload/staff");
+        String fileName = file.getOriginalFilename();
+        logger.info(" fileName = {} ",fileName);
+        logger.info(" path = {} ",path);
+        fileName = IDUtils.getId()+"_"+fileName;
+
+        File dic = new File(path);
+        if(!dic.exists()){
+            dic.mkdirs();
+        }
+        File targetFile = new File(path+"/"+fileName);
+        File pathf = new File(path);
+        if(!pathf.exists()){
+            pathf.mkdir();
+        }
+        if(!targetFile.exists()){
+            try {
+                targetFile.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        try {
+            file.transferTo(targetFile);
+            logger.info(" targetFile.getPath() = {} ",targetFile.getPath());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        JSONObject jo = new JSONObject();
+        jo.put("msg", "上传成功");
+        jo.put("url", request.getContextPath()+"/upload/staff/"+fileName);
         return ResponseUtil.success(jo);
     }
 
