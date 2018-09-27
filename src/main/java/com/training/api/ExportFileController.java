bd4a689ce43589c3e87d1551853d992164955b7e
xnsjzhangzhonghuai <Huai2323@>
2018-09-27 22:10:05 +0800

    导出文件

---
+++ src/main/java/com/training/api/ExportFileController.java
@@ -0,0 +1,49 @@
+package com.training.api;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+
+/**
+ * 上传文件 API控制器
+ */
+@RestController
+@RequestMapping("/api/export")
+public class ExportFileController {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @RequestMapping(value = "file/{id}")
+    public ResponseEntity<String> file(@PathVariable String id, HttpServletRequest request, HttpServletResponse response, ModelMap model) {
+        logger.info(" exportContact  start File.id = {}",id);
+        try {
+            response.setContentType("application/msexcel;charset=UTF-8");// 定义输出类型
+            String fileName = "file";
+//          getResponse().setContentType("application/x-download;charset=UTF-8");
+            response.setHeader("Content-Disposition","attachment;filename="+new String(fileName.getBytes(),"iso-8859-1"));
+            //读取文件
+            String path = "";
+            InputStream in = new FileInputStream(path);
+            OutputStream out = response.getOutputStream();
+            //写文件
+            int b;
+            while((b=in.read())!= -1)
+            {
+                out.write(b);
+            }
+            in.close();
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
--- src/main/java/com/training/api/MemberRestController.java
+++ src/main/java/com/training/api/MemberRestController.java
@@ -6,20 +6,31 @@ import com.training.domain.Training;
 import com.training.service.*;
 import com.training.entity.*;
 import com.training.common.*;
+import com.training.util.IDUtils;
 import com.training.util.RequestContextHelper;
+import org.apache.commons.lang.StringUtils;
+import org.apache.poi.hssf.usermodel.*;
+import org.apache.poi.hssf.util.HSSFColor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.training.util.ResponseUtil;
 import org.springframework.http.ResponseEntity;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.*;
 import com.alibaba.fastjson.JSONObject;
+
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
+import java.io.OutputStream;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * member API控制器
@@ -249,5 +260,165 @@ public class MemberRestController {
         return memberService.logoff();
     }
 
+
+    @RequestMapping(value = "exportMember")
+    public ResponseEntity<String> exportMember(@ModelAttribute MemberQuery query , HttpServletRequest request, HttpServletResponse response, ModelMap model) {
+        logger.info(" exportContact  start ContactQuery = {}",query);
+        String path = request.getSession().getServletContext().getRealPath("/export/member");
+        logger.info(" path = {} ",path);
+        String[] headers = { "姓名", "性别","电子邮箱", "手机号", "QQ" , "微信", "公司", "地址", "邮编", "国籍", "身份证号"};
+        String fileName = "contact-"+System.currentTimeMillis()+".xls";
+        File targetFile = new File(path+"/"+ fileName);
+        File pathf = new File(path);
+        logger.info(" pathf.getPath() = {} " , pathf.getPath());
+        logger.info(" targetFile.getPath() = {} " , targetFile.getPath());
+        if(!pathf.exists()){
+            pathf.mkdir();
+        }
+        if(!targetFile.exists()){
+            try {
+                targetFile.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        logger.info(" targetFile.exists() = {} " , targetFile.exists());
+        PageRequest page = new PageRequest();
+        Map result = new HashMap();
+        String id = IDUtils.getId();
+        String url = "/api/export/file/"+id;
+        result.put("url",url);
+        return ResponseUtil.success("导出会员成功！",result);
+    }
+
+
+    public void exportMemberExcel(String title, String[] headers, List<MemberEntity> dataset, OutputStream out) {
+        // 声明一个工作薄
+        HSSFWorkbook workbook = new HSSFWorkbook();
+        // 生成一个表格
+        HSSFSheet sheet = workbook.createSheet(title);
+        // 设置表格默认列宽度为15个字节
+        sheet.setDefaultColumnWidth((short) 15);
+        // 生成一个样式
+        HSSFCellStyle style = workbook.createCellStyle();
+        // 设置这些样式
+        style.setFillForegroundColor(HSSFColor.SKY_BLUE.index);
+        style.setFillPattern(HSSFCellStyle.SOLID_FOREGROUND);
+        style.setBorderBottom(HSSFCellStyle.BORDER_THIN);
+        style.setBorderLeft(HSSFCellStyle.BORDER_THIN);
+        style.setBorderRight(HSSFCellStyle.BORDER_THIN);
+        style.setBorderTop(HSSFCellStyle.BORDER_THIN);
+        style.setAlignment(HSSFCellStyle.ALIGN_CENTER);
+        // 生成一个字体
+        HSSFFont font = workbook.createFont();
+        font.setColor(HSSFColor.VIOLET.index);
+        font.setFontHeightInPoints((short) 12);
+        font.setBoldweight(HSSFFont.BOLDWEIGHT_BOLD);
+        // 把字体应用到当前的样式
+        style.setFont(font);
+        // 生成并设置另一个样式
+        HSSFCellStyle style2 = workbook.createCellStyle();
+        style2.setFillForegroundColor(HSSFColor.LIGHT_YELLOW.index);
+        style2.setFillPattern(HSSFCellStyle.SOLID_FOREGROUND);
+        style2.setBorderBottom(HSSFCellStyle.BORDER_THIN);
+        style2.setBorderLeft(HSSFCellStyle.BORDER_THIN);
+        style2.setBorderRight(HSSFCellStyle.BORDER_THIN);
+        style2.setBorderTop(HSSFCellStyle.BORDER_THIN);
+        style2.setAlignment(HSSFCellStyle.ALIGN_CENTER);
+        style2.setVerticalAlignment(HSSFCellStyle.VERTICAL_CENTER);
+        // 生成另一个字体
+        HSSFFont font2 = workbook.createFont();
+        font2.setBoldweight(HSSFFont.BOLDWEIGHT_NORMAL);
+        // 把字体应用到当前的样式
+        style2.setFont(font2);
+        // 声明一个画图的顶级管理器
+        HSSFPatriarch patriarch = sheet.createDrawingPatriarch();
+        // 定义注释的大小和位置,详见文档
+        HSSFComment comment = patriarch.createComment(new HSSFClientAnchor(0,0, 0, 0, (short) 4, 2, (short) 6, 5));
+        // 设置注释内容
+        comment.setString(new HSSFRichTextString("可以在POI中添加注释！"));
+        // 设置注释作者，当鼠标移动到单元格上是可以在状态栏中看到该内容.
+        comment.setAuthor("leno");
+        // 产生表格标题行
+        HSSFRow row = sheet.createRow(0);
+        for (short i = 0; i < headers.length; i++)
+        {
+            HSSFCell cell = row.createCell(i);
+            cell.setCellStyle(style);
+            HSSFRichTextString text = new HSSFRichTextString(headers[i]);
+            cell.setCellValue(text);
+        }
+        for (short i = 0; i < dataset.size(); i++)
+        {
+            try
+            {
+                HSSFRow rowContact = sheet.createRow(i+1);
+                MemberEntity member = dataset.get(i);
+                HSSFCell cell0 = rowContact.createCell(0);
+                cell0.setCellStyle(style2);
+                cell0.setCellValue(new HSSFRichTextString(member.getName()));
+
+                HSSFCell cell1 = rowContact.createCell(1);
+                cell1.setCellStyle(style2);
+                cell1.setCellValue(new HSSFRichTextString(member.getPhone()));
+
+                HSSFCell cell2 = rowContact.createCell(2);
+                cell2.setCellStyle(style2);
+                cell2.setCellValue(new HSSFRichTextString(member.getEmail()));
+
+                HSSFCell cell3 = rowContact.createCell(3);
+                cell3.setCellStyle(style2);
+                cell3.setCellValue(new HSSFRichTextString(member.getPhone()));
+
+                HSSFCell cell4 = rowContact.createCell(4);
+                cell4.setCellStyle(style2);
+                cell4.setCellValue(new HSSFRichTextString(member.getPhone()));
+
+                HSSFCell cell5 = rowContact.createCell(5);
+                cell5.setCellStyle(style2);
+                cell5.setCellValue(new HSSFRichTextString(member.getPhone()));
+
+                HSSFCell cell6 = rowContact.createCell(6);
+                cell6.setCellStyle(style2);
+                cell6.setCellValue(new HSSFRichTextString(member.getPhone()));
+
+                HSSFCell cell7 = rowContact.createCell(7);
+                cell7.setCellStyle(style2);
+                cell7.setCellValue(new HSSFRichTextString(member.getAddress()));
+
+                HSSFCell cell8 = rowContact.createCell(8);
+                cell8.setCellStyle(style2);
+                cell8.setCellValue(new HSSFRichTextString(member.getPhone()));
+
+                HSSFCell cell9 = rowContact.createCell(9);
+                cell9.setCellStyle(style2);
+                cell9.setCellValue(new HSSFRichTextString(member.getPhone()));
+
+                HSSFCell cell10 = rowContact.createCell(10);
+                cell10.setCellStyle(style2);
+                cell10.setCellValue(new HSSFRichTextString(member.getIdCard()));
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+        try
+        {
+            workbook.write(out);
+            out.flush();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                out.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
 
