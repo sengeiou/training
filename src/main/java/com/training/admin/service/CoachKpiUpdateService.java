4f8c2fa90d7d24baaf75387593a79e8bad3e74a0
huai23 <huai23@qq.com>
2018-10-06 00:09:50 +0800

    kpi

--- src/main/java/com/training/admin/service/CoachKpiUpdateService.java
+++ src/main/java/com/training/admin/service/CoachKpiUpdateService.java
@@ -4,10 +4,15 @@ import com.training.dao.ContractDao;
 import com.training.dao.MemberDao;
 import com.training.dao.StaffDao;
 import com.training.service.MemberCardService;
+import com.training.util.ut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class CoachKpiUpdateService {
@@ -26,9 +31,27 @@ public class CoachKpiUpdateService {
     @Autowired
     private MemberCardService memberCardService;
 
+    @Autowired
+    private CalculateKpiService calculateKpiService;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     public void execute() {
-
-
+        logger.info(" calculateKpi   ");
+        String month = ut.currentFullMonth().replace("-","");
+        List<Map<String,Object>> coachs =  jdbcTemplate.queryForList(" SELECT staff_id from staff where job = '教练' ");
+        for (int i = 0; i < coachs.size(); i++){
+            Map staff = coachs.get(i);
+            String staffId = staff.get("staff_id").toString();
+            calculateKpiService.calculateStaffKpi(staffId,month);
+        }
+        List<Map<String,Object>> managers =  jdbcTemplate.queryForList(" SELECT staff_id from staff where job = '店长' ");
+        for (int i = 0; i < managers.size(); i++){
+            Map staff = managers.get(i);
+            String staffId = staff.get("staff_id").toString();
+            calculateKpiService.calculateStaffKpi(staffId,month);
+        }
     }
 
 }
