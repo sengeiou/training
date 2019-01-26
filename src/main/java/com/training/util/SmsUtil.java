e03b97025041fb5c1bfd9dc7ab7a985653c179b4
huai45 <huai23@qq.com>
2019-01-27 00:07:26 +0800

    出勤提醒_会员修正190125

--- src/main/java/com/training/util/SmsUtil.java
+++ src/main/java/com/training/util/SmsUtil.java
@@ -232,7 +232,7 @@ public class SmsUtil {
         //必填:短信签名-可在短信控制台中找到
         request.setSignName("HeyHeroes健身");
         //必填:短信模板-可在短信控制台中找到
-        request.setTemplateCode("SMS_155455788");
+        request.setTemplateCode("SMS_157276515");
         //可选:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
         request.setTemplateParam("{\"day\":\""+day+"\"}");
         //选填-上行短信扩展码(无特殊需求用户请忽略此字段)
